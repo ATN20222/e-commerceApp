1480b7c940255fb0ba5fc1f12e190f67992a105d
amira_nasser20 <amira20nasser@gmail.com>
2023-08-30 17:31:56 +0200

    only design of favorite screen

--- ecommerce/lib/constants/app_colors.dart
+++ ecommerce/lib/constants/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   AppColors._();
   static const Color orange = Color(0xFFFAD16B);
   static const Color black = Color(0xFF282828);
+  static const Color lightgray = Color(0xFFF1F6F7);
 }
